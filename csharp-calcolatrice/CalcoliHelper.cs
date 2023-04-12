10f19eb2e849de28496d5ccf4e6b2a6c6de3bfdf
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-12 14:32:20 +0200

    ex complete

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        //metodi per la somma
+        public static int Somma(int x, int y) => x + y; 
+        public static double Somma(double x, double y) => x + y;
+
+        //metodi per la sottrazione
+        public static int Diff(int x, int y) => x - y;
+        public static double Diff(double x, double y) => x - y;
+
+        //metodi per la moltiplicazione 
+        public static int Molt(int x, int y) => x * y;
+        public static double Molt( double x, double y) => x * y;
+
+        //metodi per la divisione 
+        public static int Divisione(int x, int y) => x / y;
+        public static double Divisione( double x, double y) => x / y;
+
+        //metodo per il numero minore
+        public static int Min(int x, int y) => x < y ? x : y;
+        public static double Min(double x, double y) => x < y ? x : y;
+
+        //metodo per il massimo
+        public static int Max(int x, int y) => x > y ? x : y;
+        public static double Max(double x, double y) => x > y ? x : y;
+
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //chiedo all'utente di inserire due numeri interi
+            Console.WriteLine("Inserisci due numeri interi: ");
+            Console.Write("1. numero: ");
+            int x;
+            while (!int.TryParse(Console.ReadLine(), out x))
+                Console.WriteLine("Inserisci dei numeri");
+
+            Console.Write("2. numero: ");
+            int y;
+            while (!int.TryParse(Console.ReadLine(), out y))
+                Console.WriteLine("Inserisci dei numeri");
+
+            //chiedo all'utente di inserire due numeri con la virgola
+            Console.WriteLine("Inserisci due numeri con la virgola: ");
+            Console.Write("1. numero: ");
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a))
+                Console.WriteLine("Inserisci dei numeri");
+
+            Console.Write("2. numero: ");
+            double b;
+            while (!double.TryParse(Console.ReadLine(), out b))
+                Console.WriteLine("Inserisci dei numeri");
+
+            //SOMMA
+            Console.WriteLine($"Somma numeri interi: {CalcoliHelper.Somma(x, y)}");
+            Console.WriteLine($"Somma numeri double: {CalcoliHelper.Somma(a, b)}");
+
+            //DIFFERENZA
+            Console.WriteLine($"Differenza numeri interi: {CalcoliHelper.Diff(x, y)}");
+            Console.WriteLine($"Differenza numeri double: {CalcoliHelper.Diff(a, b)}");
+
+            //MOLTIPLICAZIONE
+            Console.WriteLine($"Moltiplicazione numeri interi: {CalcoliHelper.Molt(x, y)}");
+            Console.WriteLine($"Moltiplicazione numeri double: {CalcoliHelper.Molt(a, b)}");
+
+            //VALORE ASSOLUTO
+            Console.WriteLine($"Divisione numeri interi: {CalcoliHelper.Divisione(x, y)}");
+            Console.WriteLine($"Divisione numeri double: {CalcoliHelper.Divisione(a, b)}");
+
+            //MINIMO
+            Console.WriteLine($"Minimo tra due numeri interi: {CalcoliHelper.Min(x, y)}");
+            Console.WriteLine($"Minimo tra due numeri double: {CalcoliHelper.Min(a, b)}");
+
+            //MASSIMO
+            Console.WriteLine($"Massimo tra due numeri interi: {CalcoliHelper.Max(x, y)}");
+            Console.WriteLine($"Massimo tra due numeri double: {CalcoliHelper.Max(a, b)}");
+
         }
     }
 }
